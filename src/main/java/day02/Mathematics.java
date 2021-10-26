00bd7135ee6abb179d3101b42100f9ad7d356329
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-26 15:44:46 +0200

    add the isprime logic

---
+++ src/main/java/day02/Mathematics.java
@@ -0,0 +1,22 @@
+package day02;
+
+public class Mathematics {
+
+
+    private boolean isPrime = true;
+
+    public boolean numberIsPrime(int number) {
+        for (int i = 2; i < number; i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        Mathematics mathematics = new Mathematics();
+
+        System.out.println(mathematics.numberIsPrime(7));
+    }
+}
