893e61c8dbece1cd2e37abc59cff44e70dc1b72b
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-26 16:13:16 +0200

    fixed prime logic

--- src/main/java/day02/Mathematics.java
+++ src/main/java/day02/Mathematics.java
@@ -6,7 +6,7 @@ public class Mathematics {
     private boolean isPrime = true;
 
     public boolean numberIsPrime(int number) {
-        for (int i = 2; i < number; i++){
+        for (int i = 2; i < number / 2; i++){
             if(number % i == 0){
                 return false;
             }
